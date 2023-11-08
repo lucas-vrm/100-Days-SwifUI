701b1186e5be6f1bed52950fe67d5fad8e3389d3
lucas-vrm <luccas_menezzes@hotmail.com>
2023-11-07 23:01:53 -0300

    Day 20 – Project 2, part one

---
+++ 2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Lucas Vieira on 07/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            
+            Text("Your content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Lucas Vieira on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
