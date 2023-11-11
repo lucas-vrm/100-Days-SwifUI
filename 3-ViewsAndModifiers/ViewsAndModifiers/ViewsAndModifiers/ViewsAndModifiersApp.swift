8ba0602d608242b25f7d1a6afd8fb2b58ef2d439
lucas-vrm <luccas_menezzes@hotmail.com>
2023-11-10 22:57:03 -0300

    Day 23 – Project 3, part one

---
+++ 3-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Lucas Vieira on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
